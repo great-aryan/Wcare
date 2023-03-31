6dc37dc3b1e2c90d81d9b438833c2155c03bfd63
Aryan <aryans2808@gmail.com>
2023-03-31 17:13:51 +0530

    Fully functional

--- app/src/main/java/com/wcareteam/wcare/Contacts.kt
+++ app/src/main/java/com/wcareteam/wcare/Contacts.kt
@@ -1,59 +1,104 @@
 package com.wcareteam.wcare
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Contacts.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Contacts : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 1001
+        private const val REQUEST_SMS_PERMISSION = 1002
+    }
+
+    private val CALL_PERMISSION_REQUEST_CODE = 1
+    private fun makePhoneCall(phoneNumber: String) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CALL_PHONE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.CALL_PHONE),
+                CALL_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(callIntent)
         }
     }
+
+        override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+            if (requestCode == CALL_PERMISSION_REQUEST_CODE) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted
+                    // try to make the phone call again
+                    makePhoneCall("1234567890")
+                } else {
+                    // permission denied
+                    // show a message to the user
+                    Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false)
+        val view = inflater.inflate(R.layout.fragment_contacts, container, false)
+
+
+        val contactbutton1 = view.findViewById<Button>(R.id.contact_button1)
+//        val contactbutton2 = view.findViewById<Button>(R.id.contact_button2)
+//        val contactbutton3 = view.findViewById<Button>(R.id.contact_button3)
+//        val contactbutton4 = view.findViewById<Button>(R.id.contact_button4)
+//        val contactbutton5 = view.findViewById<Button>(R.id.contact_button5)
+
+
+        contactbutton1.setOnClickListener{
+            Log.d("MyApp", "Button clicked!")
+            makePhoneCall(getString(R.string.Contact_number1))
+
+        }
+//        contactbutton2.setOnClickListener{ makePhoneCall(getString(R.string.Contact_number2)) }
+//        contactbutton3.setOnClickListener{ makePhoneCall(getString(R.string.Contact_number3)) }
+//        contactbutton4.setOnClickListener{ makePhoneCall(getString(R.string.Contact_number4)) }
+//        contactbutton5.setOnClickListener{ makePhoneCall(getString(R.string.Contact_number5)) }
+
+
+        return view
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Contacts.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Contacts().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/wcareteam/wcare/Home.kt
+++ app/src/main/java/com/wcareteam/wcare/Home.kt
@@ -1,8 +1,10 @@
 package com.wcareteam.wcare
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.telephony.SmsManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.airbnb.lottie.LottieAnimationView
 
@@ -17,6 +20,9 @@ import com.airbnb.lottie.LottieAnimationView
 class Home : Fragment() {
     private var switchOn: Boolean = false
     var isButtonClicked = false
+    companion object {
+        private const val REQUEST_SMS_PERMISSION = 1001
+    }
 
 
       override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +44,10 @@ class Home : Fragment() {
             val handler = Handler()
             handler.postDelayed(object : Runnable {
                 override fun run() {
-                    val endIndex = (i + 2).coerceAtMost(words.size) // End index of the current batch of words
-                    val word = words.subList(i, endIndex).joinToString(" ") // Join the current batch of words into a single string
+                    val endIndex =
+                        (i + 2).coerceAtMost(words.size) // End index of the current batch of words
+                    val word = words.subList(i, endIndex)
+                        .joinToString(" ") // Join the current batch of words into a single string
                     textView.text = textView.text.toString() + " " + word
                     i = endIndex
                     if (i < words.size) {
@@ -53,12 +61,12 @@ class Home : Fragment() {
         // Find the button in the layout
         val lottieswitch = view.findViewById<LottieAnimationView>(R.id.lottieSwitch)
         val lottieswitchOff = view.findViewById<LinearLayout>(R.id.lottieSwitchOff)
-        val consoletext= view.findViewById<TextView>(R.id.console_text)
-        val hinttext= view.findViewById<TextView>(R.id.hintText)
+        val consoletext = view.findViewById<TextView>(R.id.console_text)
+        val hinttext = view.findViewById<TextView>(R.id.hintText)
         val newBackground = resources.getDrawable(R.drawable.round_bg)
         val clickAnimation = AnimationUtils.loadAnimation(context, R.anim.button_click_animation)
-        val textToType =  resources.getString(R.string.console_text)
-        val textToType2 =  resources.getString(R.string.console_text2)
+        val textToType = resources.getString(R.string.console_text)
+        val textToType2 = resources.getString(R.string.console_text2)
 
         lottieswitch.alpha = 0.0F
 
@@ -73,17 +81,17 @@ class Home : Fragment() {
                 lottieswitch.isEnabled = false // Disable the button
 
                 lottieswitch.postDelayed({
-                        lottieswitch.isEnabled = true
-                    },2000)
+                    lottieswitch.isEnabled = true
+                }, 2000)
 
-                    lottieswitch.postDelayed({
+                lottieswitch.postDelayed({
                     lottieswitch.alpha = 1.0F
                     lottieswitch.background = newBackground
                     lottieswitch.playAnimation()
 
                     switchOn = false
                     isButtonClicked = true
-                },300)
+                }, 300)
                 animateText(consoletext, textToType)
 
                 lottieswitch.postDelayed({
@@ -97,7 +105,7 @@ class Home : Fragment() {
 
                     lottieswitch.postDelayed({
                         lottieswitch.isEnabled = true
-                    },2000)
+                    }, 2000)
                     lottieswitch.alpha = 1.0F
                     lottieswitch.background = newBackground
                     lottieswitch.playAnimation()
@@ -115,7 +123,38 @@ class Home : Fragment() {
                     switchOn = true
                 }
             }
+
+
+            if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted, request it
+                requestPermissions(arrayOf(android.Manifest.permission.SEND_SMS), REQUEST_SMS_PERMISSION)
+            } else {
+                // Permission is granted, send the SMS
+                sendTestMessage()
+            }
+
         }
         return view
+    }
+    private fun sendTestMessage() {
+        // Create the message string
+        val phoneNumber = getString(R.string.Contact_number1)
+        val message = getString(R.string.urgent_message)
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_SMS_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted, send the SMS
+                    sendTestMessage()
+                } else {
+                    // Permission was denied
+                    Toast.makeText(requireContext(), "SMS permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
